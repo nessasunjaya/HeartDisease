9c4d3baefd516adfbe0ca2b53546433ef4c4bd02
Vanessa Sunjaya <83771805+nessasunjaya@users.noreply.github.com>
2023-01-10 15:34:53 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pickle
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+def predict_failure(age, sex, chestPainType, restingBP, cholesterol, fastingBS, restingECG, maxHR, exerciseAngina, oldPeak, st_Slope):
+    input = {'Age': [age], 'Sex': [sex], 'ChestPainType': [chestPainType], 'RestingBP': [restingBP], 'Cholesterol': [cholesterol], 'FastingBS': [fastingBS], 'RestingECG': [restingECG], 'MaxHR': [maxHR], 'ExerciseAngina': [exerciseAngina], 'Oldpeak': [oldPeak], 'ST_Slope': [st_Slope]}
+    
+    input = pd.DataFrame(input)
+    scaler = StandardScaler()
+    input[['Age','RestingBP','Cholesterol','MaxHR','Oldpeak']] = scaler.fit_transform(input[['Age','RestingBP','Cholesterol','MaxHR','Oldpeak']])
+    
+    prediction = model.predict(input)
+    print(type(prediction))
+    return int(prediction)
+
+def main():
+    st.title("Heart Failure Prediction")
+    html_temp = """
+        <div>
+        <h5 style="color: white;">
+        Please input with Integer for the following attributes <br>
+        </h5>
+        
+        <span style="color: #EF7C8E"> Gender </span> <br>
+        F = 0 <br>
+        M = 1 <br><br>
+        <span style="color: #EF7C8E"> Chest Pain Type </span> <br>
+        ASY = 0 <br>
+        ATA = 1 <br>
+        NAP = 2 <br><br>
+        <span style="color: #EF7C8E"> Fasting Blood Sugar </span> <br>
+        ASY = 0 <br>
+        ATA = 1 <br>
+        NAP = 2 <br><br>
+        <span style="color: #EF7C8E"> Resting ECG </span> <br>
+        FastingBS <= 120 mg/dl = 0 <br>
+        FastingBS > 120 mg/dl = 1 <br><br>
+        <span style="color: #EF7C8E"> Exercise Angina </span> <br>
+        N = 0 <br>
+        Y = 1 <br><br>
+        <span style="color: #EF7C8E"> ST Slope </span> <br>
+        Down = 0 <br>
+        Flat = 1 <br>
+        Up = 2
+        </div>
+    """
+
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    age = st.text_input("Age")
+    sex = st.text_input("Gender (Female = 0, Male = 1)")
+    chestPainType = st.text_input("Chest Pain Type (ASY = 0, ATA = 1, NAP = 2)")
+    restingBP = st.text_input("Resting Blood Pressure")
+    cholesterol = st.text_input("Cholesterol")
+    fastingBS = st.text_input("Fasting Blood Sugar (FastingBS <= 120 mg/dl = 0, FastingBS > 120 mg/dl = 1)")
+    restingECG = st.text_input("Resting Electrocardiogram Results (LVH = 0, Normal = 1, ST = 2)")
+    maxHR = st.text_input("Max Heart Rate")
+    exerciseAngina = st.text_input("Exercise Angina (No = 0, Yes = 1)")
+    oldPeak = st.text_input("Old Peak")
+    st_Slope = st.text_input("ST Slope (Down = 0, Flat = 1, Up = 2)")
+
+    safe_html = """
+        <div style="background-color: green; padding: 10px;">
+        <h2 style="color: white; text-align: center;">You don't have heart failure</h2>
+        </div>
+    """
+
+    danger_html = """
+        <div style="background-color: red; padding: 10px;">
+        <h2 style="color: white; text-align: center;">You have heart failure</h2>
+        </div>
+    """
+
+    error_html = """
+        <div style="background-color: red; padding: 10px;">
+        <h2 style="color: white; text-align: center;">Please input the correct data!</h2>
+        </div>
+    """
+
+    if st.button("Predict"):
+        output = predict_failure(age, sex, chestPainType, restingBP, cholesterol, fastingBS, restingECG, maxHR, exerciseAngina, oldPeak, st_Slope)
+
+        if output == 1:
+            st.markdown(danger_html, unsafe_allow_html=True)
+        else:
+            st.markdown(safe_html, unsafe_allow_html=True)
+
+if __name__=='__main__':
+    main()
---
+++ heart_disease_prediction.py
@@ -0,0 +1,41 @@
+# import libraries
+
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('https://raw.githubusercontent.com/nessasunjaya/KNN-to-Predict-Heart-Failure/main/heart.csv', on_bad_lines='skip')
+
+# handling categorical data
+
+columns = dataset.columns
+
+for column in columns:
+  if dataset[column].dtype == 'object':
+    dataset[column] = dataset[column].astype('category')
+    dataset[column] = dataset[column].cat.codes
+
+# Separating X (contributing value) and Y (predicted value)
+
+Y = dataset['HeartDisease']
+X = dataset.drop(['HeartDisease'], axis=1)
+
+# Standardizing Numerical Data
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X[['Age','RestingBP','Cholesterol','MaxHR','Oldpeak']] = scaler.fit_transform(X[['Age','RestingBP','Cholesterol','MaxHR','Oldpeak']])
+
+# Separating data to Training and Testing Data
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X ,Y, test_size=0.3, random_state=101)
+
+# Using KNN to Build Model
+
+from sklearn.neighbors import KNeighborsClassifier
+KNN_model = KNeighborsClassifier(n_neighbors=12)
+
+KNN_model.fit(X_train,y_train)
+KNN_predictions = KNN_model.predict(X_test)
+
+pickle.dump(KNN_model, open('model.pkl', 'wb'))
